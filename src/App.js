c218561a789fcef81576075db51f98642529c7bf
AmandaCallender <amanda.callender@outlook.com>
2020-03-19 19:46:19 -0400

    Changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,60 @@
 import React, { useState } from "react";
+import logo from "./logo.svg";
 import "./App.css";
-import Form from "./form";
-import Members from "./Members";
+import MemberList from "../src/components/MemberList";
 
 function App() {
+  //Our array of finalized members
   const [members, setMembers] = useState([]);
 
-  const addMember = member => {
-    setMembers([...members, member]);
-    console.log(members);
+  //a temp member based on what is currently entered in the input fields
+  const [member, setMember] = useState({ name: "", email: "", role: "" });
+
+  const handleChange = event => {
+    setMember({ ...member, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    members.push(member);
+    setMember({ name: "", email: "", role: "" });
   };
 
   return (
-    <div>
-      <Form addMember={addMember} />
-      <Members members={members} />
+    <div className="App">
+      <form onSubmit={event => handleSubmit(event)}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={member.name}
+            onChange={event => handleChange(event)}
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            type="text"
+            name="email"
+            value={member.email}
+            onChange={event => handleChange(event)}
+          />
+        </label>
+
+        <label>
+          Role:
+          <input
+            type="text"
+            name="role"
+            value={member.role}
+            onChange={event => handleChange(event)}
+          />
+        </label>
+        <button>Submit!</button>
+      </form>
+
+      <MemberList members={members} />
     </div>
   );
 }
